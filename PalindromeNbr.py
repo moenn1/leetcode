49d6185b4f10f63f11159610cf93e40128fa30d2
mohamed <mohamed.enn2001@gmail.com>
2022-06-30 21:33:18 +0100

    init

--- PalindromeNbr.py
+++ PalindromeNbr.py
@@ -1,3 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        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
+        temp = x
+        count = 0
+        while(x>0):
+            count = count*10+x%10
+            x=x//10
+        if temp==count:
+            return True
+        else:
+            return False
